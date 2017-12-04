3e2e950e2090f626a00b3173c7a92d3498e0e521
owen9179 <bruce19970615@gmail.com>
2017-12-04 18:44:49 +0800

    work1

--- examples/AdafruitDHT.py
+++ examples/AdafruitDHT.py
@@ -26,24 +26,24 @@ import time
 import sys
 import httplib, urllib
 import json
-deviceId = "Do0e4SFa"
-deviceKey = "vkILIBPxtr1yONXe" 
-def post_to_mcs(payload): 
-	headers = {"Content-type": "application/json", "deviceKey": deviceKey} 
-	not_connected = 1 
-	while (not_connected):
-		try:
-			conn = httplib.HTTPConnection("api.mediatek.com:80")
-			conn.connect() 
-			not_connected = 0 
-		except (httplib.HTTPException, socket.error) as ex: 
-			print "Error: %s"
-			 # sleep 10 seconds 
-	conn.request("POST", "/mcs/v2/devices/" + deviceId + "/datapoints", json.dumps(payload), headers) 
-	response = conn.getresponse() 
-	print( response.status, response.reason, json.dumps(payload), time.strftime("%c")) 
-	data = response.read() 
-	conn.close() 
+#deviceId = "Do0e4SFa"
+#deviceKey = "vkILIBPxtr1yONXe" 
+#def post_to_mcs(payload): 
+#	headers = {"Content-type": "application/json", "deviceKey": deviceKey} 
+#	not_connected = 1 
+#	while (not_connected):
+#		try:
+#			conn = httplib.HTTPConnection("api.mediatek.com:80")
+#			conn.connect() 
+#			not_connected = 0 
+#		except (httplib.HTTPException, socket.error) as ex: 
+#			print "Error: %s"
+#			 # sleep 10 seconds 
+#	conn.request("POST", "/mcs/v2/devices/" + deviceId + "/datapoints", json.dumps(payload), headers) 
+#	response = conn.getresponse() 
+#	print( response.status, response.reason, json.dumps(payload), time.strftime("%c")) 
+#	data = response.read() 
+#	conn.close() 
 
 
 # Parse command line parameters.
@@ -72,16 +72,16 @@ else:
 while True:
 	humidity, temperature = Adafruit_DHT.read_retry(sensor, pin)
 
-	h0, t0= Adafruit_DHT.read_retry(sensor, pin)
+#	h0, t0= Adafruit_DHT.read_retry(sensor, pin)
 	if humidity is not None and temperature is not None:
 		print('Temp={0:0.1f}*  Humidity={1:0.1f}%'.format(temperature, humidity))
 
-		payload = {"datapoints":[{"dataChnId":"Humidity","values":{"value":h0}},
-			{"dataChnId":"Temperature","values":{"value":t0}}]} 
-		post_to_mcs(payload)
-		time.sleep(10) 
+#		payload = {"datapoints":[{"dataChnId":"Humidity","values":{"value":h0}},
+#			{"dataChnId":"Temperature","values":{"value":t0}}]} 
+#		post_to_mcs(payload)
+		time.sleep(1) 
 
 	else:
 		print('Failed to get reading. Try again!')
-		sys.exit(1)
+sys.exit(1)
 
